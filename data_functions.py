7d4856d09a4df10cc58588f57f9c8c7b4b1b7623
MEGAjosni <jonassoeborgnielsen@gmail.com>
2021-09-30 20:09:49 +0200

    Update data_functions.py

--- data_functions.py
+++ data_functions.py
@@ -33,17 +33,17 @@ def ReadDataFromCsvFile(
     
     # Read csv file into pandas dataframe
     data = pd.read_csv(file_path)
-    data = data.drop(["Country_code", "WHO_region"], axis=1)
+    data = data.drop(["Country_code"], axis=1)
 
     # >>> Data preperation <<<
     attribute_names = data.columns
     attribute_dict = dict(zip(attribute_names, range(len(attribute_names))))
     
-    # country_codes = [code for code in list(set(data["Country_code"])) if (type(code) == str and not str.isspace(code))]
     country_names = [name for name in list(set(data["Country"])) if (type(name) == str and not str.isspace(name))]
-    
     country_dict = dict(zip(country_names, range(len(country_names))))
-    # country_dict.update(dict(zip(country_codes, range(len(country_codes))))) # Maybe to be used later...
+    
+    WHOregions = [name for name in list(set(data["WHO_region"])) if (type(name) == str and not str.isspace(name))]
+    WHOregion_dict = dict(zip(WHOregions, range(len(WHOregions))))
     
     dates = sorted(set(data[attribute_names[0]]))
     date_dict = dict(zip(dates, range(len(dates))))
@@ -51,12 +51,6 @@ def ReadDataFromCsvFile(
     data.replace({"Country": country_dict, "Date_reported": date_dict}, inplace=True)
     data = data.to_numpy()
 
-    return data, attribute_dict, date_dict, country_dict
+    return data, attribute_dict, date_dict, country_dict, WHOregion_dict
 
-import numpy as np
-
-DownloadData()
-
-data, attribute_dict, date_dict, country_dict = ReadDataFromCsvFile("Covid19_data_daily_by_country.csv")
-
-print(data[np.where(data[:, attribute_dict["Country"]] == country_dict["Denmark"])])
+ReadDataFromCsvFile("Covid19_data_daily_by_country.csv")
