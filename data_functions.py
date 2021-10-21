95d8da926282e9d59f3d859087caea2ea7cba41e
albertbrinckerolson <alboarddk@gmail.com>
2021-10-21 12:00:29 +0200

    getsynth

---
+++ data_functions.py
@@ -0,0 +1,121 @@
+    
+def DownloadData(
+        url = 'https://covid19.who.int/WHO-COVID-19-global-data.csv',
+        save_dir = ''
+        ):
+    
+    
+    # Import relevant packages
+    import requests
+    import os
+
+    # Set cwd as either specified path or script dir
+    if save_dir:
+        os.chdir(save_dir)
+    else:
+        abspath = os.path.abspath(__file__)
+        dname = os.path.dirname(abspath)
+        os.chdir(dname)
+
+    # Download and save file
+    r = requests.get(url, allow_redirects=True)
+    url_content = r.content
+    open('Covid19_data_daily_by_country.csv', 'wb').write(url_content)
+    
+    return
+
+
+def ReadDataFromCsvFile(
+        file_path,
+        ):
+    
+    # Import relevant packages
+    import pandas as pd
+    
+    # Read csv file into pandas dataframe
+    data = pd.read_csv(file_path)
+
+    # >>> Data preperation <<<
+    attribute_names = data.columns
+    attribute_dict = dict(zip(attribute_names, range(len(attribute_names))))
+    
+    # --- Special cases ---
+    # The countrycode of Namibia is missing, and is set to NA.
+    data['Country_code'].fillna('NA', inplace=True)
+    
+    # The observations without a country accounts for a total of only 764 new positives, hence it is removed
+    data = data[data['Country'] != 'Other']
+    
+    # Country and country codes is essentially the same, thus they are paired and their columns replaced by a single column with integer keys (If one wanted, it could be considered to use actual country codes for more flexibility).
+    countries = set(zip(data['Country_code'], data['Country']))
+    country_dict = dict(zip([val for sublist in countries for val in sublist], [i for i in range(len(countries)) for _ in range(2)]))
+    data = data.drop('Country_code', axis = 1)
+    
+    # Enumerate the WHO regions
+    WHOregions = list(set(data["WHO_region"]))
+    WHOregion_dict = dict(zip(WHOregions, range(len(WHOregions))))
+    
+    # Enumerate dates
+    dates = sorted(set(data[attribute_names[0]]))
+    date_dict = dict(zip(dates, range(len(dates))))
+
+    # Use the constructed dicts to change all string values to integer values - This makes the dataset easily usable with machine learning methods.
+    data.replace({'Country': country_dict, 'Date_reported': date_dict, 'WHO_region': WHOregion_dict}, inplace=True)
+
+    # Return the processed dataframe along with the necessary keys.
+    return data, attribute_dict, date_dict, country_dict, WHOregion_dict
+
+
+def ExtractContries(
+        dataframe,
+        countries,
+        country_dict
+        ):
+    
+    # Check if only 1 country is specified.
+    if type(countries) == str:
+        return dataframe.loc[dataframe['Country'] == country_dict[countries]].reset_index(drop=True)
+        
+    elif type(countries) == list:
+        # Prepare return dict
+        extracted_countries = dict()
+        for country in countries:
+            extracted_countries[country] = dataframe.loc[dataframe['Country'] == country_dict[country]].reset_index(drop=True)
+            
+        return extracted_countries
+    
+    else:
+        raise(ValueError)
+    
+
+def SIRdataframe(
+        dataframe,
+        pop = True,
+        gamma = 7,
+        dark_number_scalar = 1,
+        standardize = False
+        ):
+    
+    # Import relevant packages
+    import pandas as pd
+    
+    if pop:
+        N = 5600000 # Should be changed to actual populationvalues
+    elif type(pop) == int:
+        N = pop
+    else:
+        raise(ValueError)
+        
+    SIR_data = pd.DataFrame()
+    # SIR_data = pd.DataFrame((dataframe['Date_reported']))
+    
+    # Compute compartments
+    SIR_data['S'] = N - (dataframe['New_cases'].cumsum() * dark_number_scalar)
+    SIR_data['I'] = dataframe['New_cases'].rolling(min_periods=1, window=gamma).sum().astype('int64') * dark_number_scalar
+    SIR_data['R'] = N - (SIR_data['S'] + SIR_data['I'])
+     
+    if standardize:
+        SIR_data = (SIR_data - SIR_data.mean())/SIR_data.std()
+    
+    return SIR_data
+    
--- get_synth_data.py
+++ get_synth_data.py
@@ -4,34 +4,60 @@ Created on Wed Sep 22 09:08:13 2021
 
 @author: alboa
 """
-import basic_ivp_funcs as bif
-import expanded_ivp_funcs as eif
+
+#import sim_functions as sf
 import matplotlib.pyplot as plt
+from models import SIR
+from models import S3I3R
 import numpy as np
 
-def Create_synth_data(X_0: np.array, #Initial values
-                      mp: list, # model parameters
-                      model_type = 'basic', # either basic or expanded
-                      simdays: int = 100, # length of synthetic data (in days)
-                      stepsize: float = 0.1, # t_kp1 - t_k
-                      T: list = [], # vaccination list if expanded model is chosen. default is no vaccinations
-                      noise_var: float = 0.1
+def Create_synth_data(X_0: np.array, 
+                      mp: list,
+                      t: list, 
+                      model=SIR,
+                      T: list = [], 
+                      noise_var: float = 0.1 # 
                       ):
-    # *** Description ***
-    # Produces synthetic data for either expanded or basic SIR model
+    '''
 
-    # *** Output ***
-    # SIR matrix or S3I3R matrix depending on model type 
+    Parameters
+    ----------
+    X_0 : np.array
+        Initial values                      
+    mp : list
+        model parameters.
+    model: function, optional.
+        Default is SIR. can also be set to S3I3R
+    t : list
+        Sample points.
+    T : list, optional 
+        vaccination list if expanded model is chosen. default is no vaccinations
+    noise_var : float, optional
+        
     
-    if model_type == 'basic':
+        
+
+    Returns
+    -------
+    t : 
+        DESCRIPTION.
+    TYPE
+        DESCRIPTION.
+
+    '''
+    from models import SIR
+    from models import S3I3R
+    
+    
+    if model == SIR:
         if len(mp)!=2:
             print("Incorrect model parameter dimension! (must be 1x3)")
         if len(X_0) !=3:
             print("Incorrect initial value dimension! (must be 1x3) ")
-        t,SIR = bif.simulateSIR(X_0,mp,simtime=simdays,stepsize=stepsize)
+        t,SIR = sf.SimulateModel(t,X_0,mp, model=SIR)
        
        #add noise and transform to daily data points
-        SIR_noise = np.zeros((3,simdays))
+        SIR_noise = np.zeros((3,len(t)))
         count=0    
         for i in range(0,len(SIR)):
            if i%10 == 0 and i!= 0:
@@ -44,19 +70,17 @@ def Create_synth_data(X_0: np.array, #Initial values
                
         return t,SIR_noise.transpose()
            
-    if model_type == 'expanded':
+    if model == S3I3R:
         # translates default T=[] to actual zero vector of correct length
         if len(mp)!=7:
             print("Incorrect model parameter dimension! (must be 1x7)")
         if len(X_0) !=7:
             print("Incorrect initial value dimension! (must be 1x7) ")
-        if not T:
-            T = np.zeros(simdays)
-            
-        t, S3I3R = eif.simulateSIR(X_0,mp,simtime=simdays,stepsize=stepsize,T=T)
+        
+        t, S3I3R = sf.SimulateModel(t,X_0,mp,model)
         S3I3R.transpose()
         # add noise (only to I1 category) and transform to daily data points. 
-        S3I3R_noise = np.zeros((7,simdays+1))
+        S3I3R_noise = np.zeros((7,len(t)+1))
         count=1
         for i in range(0,len(S3I3R)):
             if i%10 == 0 and i!= 0:
@@ -82,10 +106,10 @@ plt.plot(t[1:],X)
 '''
 
 # TEST EXPANDED #
-X_0 = [10000,1000,20,2,0,0,0]
-mp = [200,1/9, 1/7, 1/21,0.3,0.3,0.6]
+X_0 = [6000000,1000,20,2,0,0,0]
+mp = [1,1/9, 1/7, 1/21,0.3,0.3,0.6]
 
-t,X = Create_synth_data(X_0,mp,model_type='expanded',noise_var = 0)
+t,X = Create_synth_data(X_0,mp,model_type='expanded',noise_var = 0.1)
 t=t[::10]
 plt.plot(t,X[0,:],t,X[1,:],t,X[2,:],t,X[3,:],t,X[4,:],t,X[5,:],t,X[6,:])
 plt.legend(["S : susceptible","I1 : regular infected","I2 : ICU infected","I3 : respirator infected","R1 : regular recovered","R2 : vaccinated","R3 : dead"])
