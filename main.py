b68bfd8ae0caac2191ac1371deec68683fc2b4d3
MarcusGalea <s194336@student.dtu.dk>
2021-12-03 19:31:40 +0100

    changes

--- main.py
+++ main.py
@@ -7,29 +7,27 @@ Created on Fri Oct  1 00:17:49 2021
 
 import matplotlib.pyplot as plt
 import numpy as np
-from data_functions import *
 import get_synth_data as gsd
 import paramest_functions_OLS as pest_OLS
 from models import SIR, S3I3R, TripleRegionSIR
-from sim_functions import SimulateModel, LeastSquareModel, NoneNegativeLeastSquares
+from sim_functions import SimulateModel2, LeastSquareModel, NoneNegativeLeastSquares
 
 #initialise duration of simulation
-simdays = 100
+simdays = 150
 t1 = 0
 t2 = t1+simdays
 
 
 ############ Real time estimations ################################
 over_time = True #set "True" if estimations are needed in real time
-overshoot = 14 #the amount of previous days included for parameter estimations in real time
+overshoot = 4 #the amount of previous days included for parameter estimations in real time
 
 
 #initialize IC and parameters for the syntetic data
-X0 = [10000, 30, 0] #initial conditions
-beta= 0.23  #rate of transmission
-gamma = 0.08 #rate of recovery
+X0 = [9.99999833e-01, 1.66666667e-07, 0.00000000e+00] #initial conditions
+beta= 0.5  #rate of transmission
+gamma = 1/3     #rate of recovery
 noise_var = 0 #variance of added noise
-stepsize = 0.1 #stepsize in Runge-Kutta iteration
 
 t = np.arange(simdays)
 
@@ -44,10 +42,10 @@ include_params = True #include parameters in plot
 
 
 #Generate synthetic data or use real data
-#mp = [beta, gamma]
-mp = [[0.05*np.sin(2*np.pi/simdays*i)+0.2,gamma] for i in range(simdays)]
 
-X_syn = SimulateModel(t, X0, mp, model=SIR, realtime=over_time)
+mp = np.array([[0.05*np.sin(2*np.pi/simdays*i)+beta,gamma] for i in range(simdays)])
+
+X_syn = SimulateModel2(t, X0, mp, model=SIR, realtime=over_time)
 
 
     #real time parameter estimation from data
@@ -61,7 +59,7 @@ mp_est = pest_OLS.SIR_params_over_time_OLS(
 
 
 #simulation using retrieved parameters
-X_est = SimulateModel(t[overshoot:], X_syn[overshoot, :], mp_est, model=SIR, realtime=over_time)
+X_est = SimulateModel2(t[overshoot:], X_syn[overshoot, :], mp_est, model=SIR, realtime=over_time)
 
 
 #plots
@@ -75,21 +73,13 @@ ax.legend("S",loc="upper left")
 if include_params:
     ax2.plot(mp_est[:,0], c = "r")
     ax2.plot(mp_est[:,1], c = "tab:purple")
-    ax2.plot(t,beta*np.ones(len(t)),"--",c = "r")
-    ax2.plot(t,gamma*np.ones(len(t)),"--",c = "tab:purple")
+    if over_time:
+        ax2.plot(t,mp[:,0],"--",c = "r")
+        ax2.plot(t,mp[:,1],"--",c = "tab:purple")
+    else:
+       ax2.plot(t,beta*np.ones(len(t)),"--",c = "r")
+       ax2.plot(t,gamma*np.ones(len(t)),"--",c = "tab:purple")
     ax2.set_ylim(0,0.30)
     ax2.legend(["b est","g est","b true", "g true"],loc = "upper right")
 
-#%% Once again with real data
-
-start_date = "2020-12-01"
-#initilize SIR data
-simdays = 100
-t1 = 0
-t2 = t1+simdays
-
-data, attribute_dict, date_dict, country_dict, WHOregion_dict = ReadDataFromCsvFile("Covid19_data_daily_by_country.csv")
-X = ExtractContries(data,"Denmark",country_dict)
-X = SIRdataframe(X,gamma,dark_number_scalar=1).to_numpy()
-X = X[date_dict[start_date]:date_dict[start_date]+simdays+overshoot]
 
