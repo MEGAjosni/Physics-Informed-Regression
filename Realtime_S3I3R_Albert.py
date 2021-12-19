fc220d79330278240cf2226d1ae3af84f3c517ba
albertbrinckerolson <alboarddk@gmail.com>
2021-12-19 12:04:56 +0100

    added realtime s3i3r

--- Realtime_S3I3R_Albert.py
+++ Realtime_S3I3R_Albert.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec  1 13:46:18 2021
+Created on Sun Dec 19 11:36:14 2021
 
 @author: alboa
 """
@@ -27,45 +27,67 @@ from sim_functions import SimulateModel2, LeastSquareModel, NoneNegativeLeastSqu
 
 # SIR model parameters
 simdays = 70  # compute ground truth
-beta=0.4 
-gamma=1/3
+beta = 0.5
+gamma1 = 1/3 
+gamma2 = 1/20
+gamma3 = 1/20
+phi1 = 1/20
+phi2 = 1/20
+theta = 1/10
+tau = 0.001
 dt = 0.1
 t_true= np.arange(0, simdays)
 varying_params_est = [] 
 avg__true_params = []
 
-# parameters to be identified. There are as many betas as days
-
 
 #beta function definition (arbitrary option. I chose sinwave here)
-def mpfun(t,beta,gamma,simdays):
+def mpfun(t,beta,gamma1,gamma2,gamma3,phi1,phi2,theta,tau,simdays):
     if type(t) is int:
-        return [0.05*np.sin(2*np.pi/simdays*t)+beta,gamma]
+        return [0.05*np.sin(2*np.pi/simdays*t)+beta,gamma1,gamma2,gamma3,phi1,phi2,theta,tau]
         #return[beta,gamma]
     else:
-        return np.vstack([0.05*np.sin(2*np.pi/simdays*t)+beta,gamma*np.ones(len(t))]).T
+        return np.vstack([0.05*np.sin(2*np.pi/simdays*t)+beta,
+                          gamma1*np.ones(len(t)),
+                          gamma2*np.ones(len(t)),
+                          gamma3*np.ones(len(t)),
+                          phi1*np.ones(len(t)),
+                          phi2*np.ones(len(t)),
+                          theta*np.ones(len(t)),
+                          tau*np.ones(len(t))]).T
         #return np.vstack([beta*np.ones(len(t)),gamma*np.ones(len(t))]).T
 
 def boundary(_, on_initial):
     return on_initial
 
 
-# SIR model definition
-def SIR_system(t,z):
-    z0, z1, z2 = z[:,0:1], z[:,1:2], z[:,2:]
-    dz0_t = dde.grad.jacobian(z, t, i=0)
-    dz1_t = dde.grad.jacobian(z, t, i=1)
-    dz2_t = dde.grad.jacobian(z, t, i=2)
+# S3I3R model definition
+def S3I3R_system(t,X):
+    # note, vacination not implemented due to nan errors corrupting training. 
+    
+    S,I1,I2,I3,R1,R2,R3 = X[:,0:1], X[:,1:2], X[:,2:3],X[:,3:4],X[:,4:5],X[:,5:6],X[:,6:]
+    dS_t = dde.grad.jacobian(X, t, i=0)
+    dI1_t = dde.grad.jacobian(X, t, i=1)
+    dI2_t = dde.grad.jacobian(X, t, i=2)
+    dI3_t = dde.grad.jacobian(X, t, i=3)
+    dR1_t = dde.grad.jacobian(X, t, i=4)
+    dR2_t = dde.grad.jacobian(X, t, i=5)
+    dR3_t = dde.grad.jacobian(X, t, i=6)
+    
+    # return rhs-lhs in ode_system
     return [
-      dz0_t - ( -C1*z0*z1 )  ,
-      dz1_t - ( C1*z0*z1 - C2*z1 ),
-      dz2_t - ( C2*z1 )
-    ]
+        dS_t - (-(beta_var*I1)*S),
+        dI1_t - (beta_var*I1*S - (gamma1 + phi1_var)*I1),
+        dI2_t - (phi1_var * I1 -(gamma2+ phi2_var) * I2),
+        dI3_t - (phi2_var * I2 -(gamma3 + theta_var) * I3),
+        dR1_t - (gamma1 * I1 + gamma2 * I2 + gamma3 * I3),
+        dR2_t - 0,
+        dR3_t - theta_var * I3]
 
 
 # generate mp (with high resolution of dt, over entire domain)
 t_mp= np.arange(0, simdays,dt)
-mp = mpfun(t_mp,beta,gamma,simdays)
+mp = mpfun(t_mp,beta,gamma1,gamma2,gamma3,phi1,phi2,theta,tau,simdays)
 
 
 # Generate true solution
@@ -84,10 +106,13 @@ for day in range(1,simdays):
     t_0 = day-days_for_est
     
     print(day)
-    C1 = tf.Variable(0.5)
-    C2 = tf.Variable(0.5)
-    # trains a model for each day, taking the previous {days_for_est} number of days as training data
     
+    # trains a model for each day, taking the previous {days_for_est} number of days as training data
+    beta_var = tf.Variable(0.5)
+    phi1_var = tf.Variable(0.5)
+    phi2_var = tf.Variable(0.5)
+    theta_var = tf.Variable(0.5)
+
 
     # define train data for relevant days used as training for this particular day
     t_test = np.arange(t_0,day,dt) 
@@ -104,22 +129,30 @@ for day in range(1,simdays):
     
     # Initial conditions
     ic1 = dde.IC(geom, lambda X: x_true[t_0,0], boundary, component=0) # S
-    ic2 = dde.IC(geom, lambda X: x_true[t_0,1], boundary, component=1) # I
-    ic3 = dde.IC(geom, lambda X: x_true[t_0,2], boundary, component=2) # R
+    ic2 = dde.IC(geom, lambda X: x_true[t_0,1], boundary, component=1) # I1
+    ic3 = dde.IC(geom, lambda X: x_true[t_0,2], boundary, component=2) # I2
+    ic4 = dde.IC(geom, lambda X: x_true[t_0,3], boundary, component=3) # I3
+    ic5 = dde.IC(geom, lambda X: x_true[t_0,4], boundary, component=4) # R1
+    ic6 = dde.IC(geom, lambda X: x_true[t_0,5], boundary, component=5) # R2
+    ic7 = dde.IC(geom, lambda X: x_true[t_0,6], boundary, component=6) # R3
     
     
     # point boundary conditions
     observe_t = t_train
-    observe_z = x_train
-    observe_z0 = dde.PointSetBC(observe_t, observe_z[:, 0:1], component=0)
-    observe_z1 = dde.PointSetBC(observe_t, observe_z[:, 1:2], component=1)
-    observe_z2 = dde.PointSetBC(observe_t, observe_z[:, 2:], component=2)
+    observe_X = x_train
+    observe_S = dde.PointSetBC(observe_t, observe_X[:, 0:1], component=0)  # S
+    observe_I1 = dde.PointSetBC(observe_t, observe_X[:, 1:2], component=1) # I1
+    observe_I2 = dde.PointSetBC(observe_t, observe_X[:, 2:3], component=2) # I2
+    observe_I3 = dde.PointSetBC(observe_t, observe_X[:, 3:4], component=3) # I3
+    observe_R1 = dde.PointSetBC(observe_t, observe_X[:, 4:5], component=4) # R1
+    observe_R2 = dde.PointSetBC(observe_t, observe_X[:, 5:6], component=5) # R2 
+    observe_R3 = dde.PointSetBC(observe_t, observe_X[:, 6:], component=6)  # R3
     
     # define data object
     data = dde.data.PDE(
         geom,
-        SIR_system,
-        [ic1, ic2, ic3, observe_z0, observe_z1, observe_z2],
+        S3I3R_system,
+        [ic1, ic2, ic3, ic4, ic5, ic6, ic7,observe_S, observe_I1, observe_I2, observe_I3, observe_R1, observe_R2, observe_R3],
         num_domain = int(days_for_est/dt),
         num_boundary=2,
         
@@ -133,9 +166,9 @@ for day in range(1,simdays):
     #model.save(getcwd())
     
     # callbacks for storing results, and outputting the values of C1, C2 subject to parameter estimation (model calibration)
-    fnamevar = "variables_SIR.dat"
+    fnamevar = "variables_S3I3R.dat"
     variable = dde.callbacks.VariableValue(
-        [C1,C2], 
+        [beta_var,phi1_var,phi2_var,theta_var], 
         period=1,
         filename=fnamevar
     )
@@ -160,7 +193,7 @@ for day in range(1,simdays):
     #print(mp_day)
     
 # save estimated parameters    
-np.savetxt('pinn_params_SIR3.out',varying_params_est,delimiter = ',')
+np.savetxt('varying_parameters_S3I3R_v1.out',varying_params_est,delimiter = ',')
 
 
 # plot against actual parameters 
